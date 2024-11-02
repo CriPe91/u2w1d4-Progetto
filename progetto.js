3a3abc7939bf5386739ad73ab755cd0ef403dbca
CriPe91 <cristian.petta91@gmail.com>
2024-11-02 11:56:25 +0100

    last update

--- progetto.js
+++ progetto.js
@@ -1,7 +1,7 @@
 const barra = document.querySelector("header");
 const button = document.querySelector(".btn");
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 500) {
+  if (window.scrollY > 467) {
     barra.style.background = "white";
     button.style.background = "#1a8917";
   } else {
